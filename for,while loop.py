2c28d549aee04c740587e401fc7c3473ac72b955
Bibi Ayesha Momin <92103793+ayshmn@users.noreply.github.com>
2026-07-07 20:01:48 +0530

    Add files via upload

---
+++ for,while loop.py
@@ -0,0 +1,57 @@
+# for loop
+
+# 1. Print numbers from 1 to 5
+
+for i in range(1,6):
+    print(i)
+
+
+# 2. Print a name 3 times
+
+for i in range(3):
+    print("Ayesha")
+
+
+# Find sum of numbers
+
+total = 0
+
+for i in range(1,6):
+    total += i
+
+print("sum", total)
+
+
+
+# while loop
+
+
+# 1. Print numbers from 1 to 5
+
+i = 1
+
+while i <= 5:
+    print(i)
+    i += 1
+
+
+# 2. Print a number 3 times
+
+count = 1
+
+while count <= 3:
+    print("Ayesha")
+    count += 1
+
+
+# 3. Find sum of numbers
+
+
+i = 1
+total = 0
+
+while i <= 5:
+    total += i
+    i += 1
+
+print("Sum=", total)
